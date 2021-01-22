7c3dccd23b22c24914afb3f0b0eff498cc12470d
Vitor Lima <vitorlimadev@outlook.com>
2021-01-22 14:19:03 -0300

    feat: start game Agent

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,7 +1,15 @@
 defmodule ExMon do
-  alias ExMon.Player
+  alias ExMon.{Game, Player}
+
+  @computer_name "Blastoise"
 
   def create_player(name, normal_attack, strong_attack, heal) do
     Player.build(name, normal_attack, strong_attack, heal)
+  end
+
+  def start_game(player) do
+    @computer_name
+    |> create_player("Torrent", "Rain Dish", "Fish")
+    |> Game.start(player)
   end
 end
